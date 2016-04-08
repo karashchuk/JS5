729dbb8160a5be58ad182f1eb136d8d702a3785a
pantol <pantolik@rambler.ru>
2016-04-08 19:56:38 +0300

    Поменял GET на POST

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
     $(function() {
         $(document).on('click', '#send', function(event) {
             
-            $.get('validator.php',{
+            $.post('validator.php',{
                 'username': $('#username').val(),
                 'password': $('#password').val(),
                 'email': $('#email').val(),
                 'gender': $('#gender').val(),
-                'credit_card': $('credit_card').val(),
+                'credit_card': $('#credit_card').val(),
                 'bio': $('#bio').val(),
             },
               function(data){
